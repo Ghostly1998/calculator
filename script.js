6b42f8c5511705759728d8f5cb4af13a81334af8
ghostly1998 <bkanwal1998@gmail.com>
2022-12-28 12:43:32 +0530

    do simple calculation based on user input and display the results

--- script.js
+++ script.js
@@ -1,73 +1,62 @@
 const digitButton = document.querySelectorAll('#digit');
-const addBtn = document.getElementById('add-btn');
-const subBtn = document.getElementById('subtract-btn');
-const mulBtn = document.getElementById('multiply-btn');
-const divBtn = document.getElementById('divide-btn');
-const clrBtn = document.getElementById('clear-btn');
-const equalBtn = document.getElementById('equal-btn');
 const displayArea = document.getElementById('displayarea');
+const clearBtn = document.getElementById('clear-btn');
+const equalBtn = document.getElementById('equal-btn');
+const delBtn = document.getElementById('del-btn');
 
-
-
-function add(...num){
-    // console.log(num);
-    let finalValue = null;
-    num.forEach(item => {
-        // console.log(item);
-        finalValue += item;
-    });
-    console.log(addition)
-    return addition; 
+// functions for calculating based on user input
+function add (a, b) {
+  return a + b;
 }
-
-// add(1,2,4,3);
-
-function subtract(...num){
-    let finalValue = null;
-    num.forEach(item => {
-        finalValue = -finalValue - item;
-    });
-    console.log(finalValue);
-    return finalValue;
+function subtract (a, b) {
+  return a - b;
 }
-// subtract(7,13)
-
-
-function multiply(...num){
-    let finalValue = 1;
-    num.forEach(item => {
-        finalValue *= item;
-    });
-    console.log(finalValue);
-    return finalValue;
+function multiply (a, b) {
+  return a * b;
 }
-// multiply(6,5)
-
-function divide(...num){
-    let finalValue = 1;
-    finalValue = num[0]/num[1];
-    
-    console.log(finalValue);
-    return finalValue;
+function divide (a, b) {
+  if (b === 0) {
+    displayArea.value = "Can't divde by zero!"
+  } else return a / b;
 }
+// send user entered data to appropriate function
+// for calculation and return result
+function operate (operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+}
+// event listener for displaying user input
+digitButton.forEach(item => {
+  item.addEventListener('click', () => {
+    displayArea.value += item.value;
+  })
+})
+// event listener for clearing all data on display
+clearBtn.addEventListener('click', () => {
+  displayArea.value = '';
+})
+// for performing calculation when equal is pressed
+equalBtn.addEventListener('click', () => {
+  displayArea.value = calculate(displayArea.value)
+})
+// delete last element for view
+delBtn.addEventListener('click', () => {
+  displayArea.value = displayArea.value.slice(0, -1);
+})
 
-// divide(8,2)
-
-function operate(operater, ...num) {
-    switch(operate){
-        case '+' :
-            add(...num);
-            break;
-        
-        case '-' :
-            subtract(...num);
-            break;
-        case '*' :
-            multiply(...num);
-            break;
-        case '/' :
-            divide(...num);
-            break;
-
-    }
+// convert string to numbers and operator and send for calculation
+function calculate (someString) {
+  let newArray = someString.split(' ');
+  let num1 = Number(newArray[0])
+  let num2 = Number(newArray[2])
+  let operator = newArray[1];
+  return operate(operator, num1, num2);
 }
